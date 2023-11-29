7224be2510764ae3fbf9f79e1635630e84d74ef8
goose <goose@localhost.localdomain>
2023-11-29 09:43:49 -0500

    Started html outline of website, with styling.

---
+++ index.js
@@ -0,0 +1,9 @@
+const brand = document.getElementsById('brand');
+const cart = document.getElementsById('cart');
+const electronics = document.getElementsById('electronics');
+const jewelry = document.getElementsById('jewelry');
+const men = document.getElementsById('men');
+const women = document.getElementsById('women');
+const display = document.getElementById('display');
+
+const URL = 'https://fakestoreapi.com';
